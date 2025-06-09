8e91ec211ed95501964a415e30cb7cd5356b79b3
alvieg <168017188+alvieg@users.noreply.github.com>
2025-06-09 18:25:49 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-// filepath: vite.config.js
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  plugins: [svelte()],
-});
